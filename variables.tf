4b52d048e6619f405dee8e6d07bb29eb957b5cc2
NaGaii1994 <nagai_akihiro@icloud.com>
2022-11-22 00:01:58 +0900

    [feat]Add project_name to variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "The AWS region to create resources in."
   default     = "ap-northeast-1"
 }
+
+variable "project_name" {
+  description = "The AWS region to create resources in."
+  default     = "ACMRoute53Practice"
+}
