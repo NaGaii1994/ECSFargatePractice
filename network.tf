ce42eb1a2942c2a2b9a7fb3e24acc3c2feb78587
NaGaii1994 <nagai_akihiro@icloud.com>
2022-11-23 19:13:46 +0900

    [feat]Add 2 private subnet, nat-gw and eip and that of rtb.

--- network.tf
+++ network.tf
@@ -27,6 +27,55 @@ resource "aws_subnet" "public_subnet_c" {
   }
 }
 
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "ap-northeast-1a"
+  tags = {
+    Name = "${var.project_name}_public_subnet_a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "${var.project_name}_public_subnet_c"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table_association" "private_subnet_a_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet_a.id
+}
+resource "aws_route_table_association" "private_subnet_c_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet_c.id
+}
+
+resource "aws_eip" "elastic_ip_for_nat_gw" {
+  vpc                       = true
+  associate_with_private_ip = "10.0.0.5"
+  depends_on                = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic_ip_for_nat_gw.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+  depends_on    = [aws_eip.elastic_ip_for_nat_gw]
+}
+
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
